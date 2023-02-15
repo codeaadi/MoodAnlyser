d638d590126a5e5b30b1e3294c1955f3a00a7143
codeaadi <adityadubey362@gmail.com>
2023-02-15 08:00:56 +0530

    Added: Program to find sad mood

---
+++ src/com/bridgelabz/MoodAnalyser/src/main/java/org/Moodanalyser/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.MoodAnalyser.src.main.java.org.Moodanalyser;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To MoodAnalyser");
+    }
+}
---
+++ src/com/bridgelabz/MoodAnalyser/src/main/java/org/Moodanalyser/MoodAnalysisException.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.MoodAnalyser.src.main.java.org.Moodanalyser;
+
+
+    public class MoodAnalysisException extends Exception {
+
+        public MoodAnalysisException(String msg){
+            super(msg);
+        }
+    }
+
